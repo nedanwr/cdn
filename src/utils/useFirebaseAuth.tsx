830ffa73da598a304c4e3f94d19ae936bee164c9
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 11:31:14 +0200

    Feat: add auth & create signInWithEmailAndPassword

--- src/utils/useFirebaseAuth.tsx
+++ src/utils/useFirebaseAuth.tsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import "./initFirebase";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut
+} from "firebase/auth";
 
 const formatAuthUser = (user: any) => ({
     uid: user.uid,
@@ -10,6 +16,9 @@ export const useFirebaseAuth = () => {
     // State
     const [authUser, setAuthUser] = useState<any>(null);
     const [loading, setLoading] = useState<boolean>(true);
+
+    // Set Auth
+    const auth = getAuth();
 
     const authStateChanged = async (authState: any) => {
         if (!authState) {
@@ -30,4 +39,8 @@ export const useFirebaseAuth = () => {
         setAuthUser(null);
         setLoading(true);
     };
+
+    const signInWithEmailAndPassword = async (email: string, password: string) => {
+        await signInWithEmailAndPassword(email, password);
+    }
 }
