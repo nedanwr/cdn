ce0c7eedfadd1997e5e362792517ac919205aac5
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 11:27:09 +0200

    Feat: create authStateChanged

--- src/utils/useFirebaseAuth.tsx
+++ src/utils/useFirebaseAuth.tsx
@@ -5,3 +5,24 @@ const formatAuthUser = (user: any) => ({
     uid: user.uid,
     email: user.email,
 });
+
+export const useFirebaseAuth = () => {
+    // State
+    const [authUser, setAuthUser] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const authStateChanged = async (authState: any) => {
+        if (!authState) {
+            setLoading(false);
+            return;
+        }
+
+        setLoading(true);
+
+        let formattedUser = formatAuthUser(authState);
+
+        setAuthUser(formattedUser);
+
+        setLoading(false);
+    };
+}
