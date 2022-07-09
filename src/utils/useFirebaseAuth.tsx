7a0d49196d172b0dd20c0aff9c950c5ea0205f6e
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 14:10:34 +0200

    Feat: config use firebase auth

--- src/utils/useFirebaseAuth.tsx
+++ src/utils/useFirebaseAuth.tsx
@@ -1,12 +1,6 @@
 import { useState, useEffect } from "react";
 import "./initFirebase";
-import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signOut
-} from "firebase/auth";
-import type { Auth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const formatAuthUser = (user: any) => ({
     uid: user.uid,
@@ -18,38 +12,31 @@ export const useFirebaseAuth = () => {
     const [authUser, setAuthUser] = useState<any>(null);
     const [loading, setLoading] = useState<boolean>(true);
 
-    // Set Auth
-    const auth: Auth = getAuth();
+    // Auth
+    const auth = getAuth();
 
     const authStateChanged = async (authState: any) => {
         if (!authState) {
+            setAuthUser(null);
             setLoading(false);
             return;
         }
 
         setLoading(true);
 
-        let formattedUser = formatAuthUser(authState);
-
+        const formattedUser = formatAuthUser(authState);
         setAuthUser(formattedUser);
-
         setLoading(false);
     };
 
-    const clear = (): void => {
-        setAuthUser(null);
-        setLoading(true);
+    // Listen for Firebase auth state changes
+    useEffect(() => {
+       const unsubscribe = onAuthStateChanged(auth, authStateChanged);
+       return () => unsubscribe();
+    }, [auth]);
+
+    return {
+        authUser,
+        loading,
     };
-
-    const logIn = async (email: string, password: string) => {
-        await signInWithEmailAndPassword(auth, email, password);
-    }
-
-    const createUser = async (email: string, password: string) => {
-        await createUserWithEmailAndPassword(auth, email, password);
-    }
-
-    const logOut = async () => {
-        await signOut(auth).then(clear);
-    }
 }
