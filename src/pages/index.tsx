46781b94903a1463376586b1f740bb5ad76b765c
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 14:34:45 +0200

    Feat: add logout functionality & add WaitForAuth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import { WaitForAuth } from "../modules/auth/WaitForAuth";
+import { getAuth, signOut } from "firebase/auth";
 
 const Home: NextPage = () => {
-    return <h1>Hello World</h1>
+    const logOut = async () => {
+        const auth = getAuth();
+
+        await signOut(auth);
+    }
+
+    return (
+        <WaitForAuth>
+            <h1>Hello World</h1>
+            <button onClick={logOut}>Logout</button>
+        </WaitForAuth>
+    )
 }
 
 export default Home;
