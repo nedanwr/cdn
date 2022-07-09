1cf7099b430a7897a65ace30e1a47ad8354b0906
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 22:36:28 +0200

    Feat: init multer

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -5,3 +5,11 @@ import multer from "multer";
 interface NextConnectApiRequest extends NextApiRequest {
     file: Express.Multer.File;
 }
+
+// Init Multer
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    }
+});
