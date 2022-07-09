cead7bbe6fe97750fd7d5b6ab5563cd597003a84
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 14:10:02 +0200

    Feat: add AuthUserProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from "next/app";
+import { AuthUserProvider } from "../context/authUserContext";
 import "../styles/globals.scss";
 import "../utils/initFirebase";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>;
 }
 
 export default App;
