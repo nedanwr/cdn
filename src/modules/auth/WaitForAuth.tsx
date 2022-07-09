dcd060ad8fa3d62059ff53a146f00ed13d819a11
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-09 14:08:08 +0200

    Feat: check if user is authenticated

--- src/modules/auth/WaitForAuth.tsx
+++ src/modules/auth/WaitForAuth.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import { getAuth, Auth, onAuthStateChanged, User } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useAuth } from "../../context/authUserContext";
 
 export const WaitForAuth = ({ children }: any) => {
-    const auth: Auth = getAuth();
-    onAuthStateChanged(auth, (user: User | null) => {
-        if (user) {
-            return <>{children}</>;
-        }
-        else {
-            if (window !== undefined) {
-                window.location.href = "/auth/login";
-            }
-        }
-    });
+    const { authUser, loading } = useAuth();
+    const router = useRouter();
+
+    // If is not authenticated, redirect to log in page
+    if (!loading && !authUser) {
+        router.push("/auth/login");
+        return <h1>Redirecting...</h1>;
+    }
+
+    return <>{children}</>;
 }
