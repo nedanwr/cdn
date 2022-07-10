50ec1529335f816bcca1c68fb027ad25ca14b3f0
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-10 17:41:55 +0200

    Feat: get user & check if uploadKey matches

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 
+// Import Utils
+import { getUser } from "./getUser";
+
 export const verifyToken = async (req: NextApiRequest, res: NextApiResponse, next: any) => {
     // Get token from header
     const bearerHeader: string | undefined = req.headers["authorization"];
@@ -22,8 +25,31 @@ export const verifyToken = async (req: NextApiRequest, res: NextApiResponse, nex
                     });
 
             // Check if user with secret key in token exists
+            getUser(decoded.uid)
+                .then((user) => {
+                    // Check if user exists
+                    if (!user) {
+                        // Send 403 status if user does not exist
+                        return res.status(403)
+                            .json({
+                                statusCode: 403,
+                                error: "Forbidden",
+                                message: "You are not authorized to access this resource."
+                            });
+                    }
 
-            next();
+                    // Check if uploadKey provided in token matches uploadKey in user
+                    if (user.uploadKey !== decoded.uploadKey)
+                        // Send 403 status if uploadKey in token does not match uploadKey in user
+                        return res.status(403)
+                            .json({
+                                statusCode: 403,
+                                error: "Forbidden",
+                                message: "You are not authorized to access this resource."
+                            });
+
+                    next();
+                });
         });
     }
 }
