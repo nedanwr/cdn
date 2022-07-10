63d176552ae23070e2e7b89b58db78aa78f8d1f8
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-10 17:38:48 +0200

    Feat: use generateUploadKey function

--- src/utils/setAdditionalUserData.ts
+++ src/utils/setAdditionalUserData.ts
@@ -1,10 +1,12 @@
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { generateUploadKey } from "../lib/generateUploadKey";
 
 export const setAdditionalUserData = async (uid: string, displayName: string) => {
     const firestore = getFirestore();
 
     await setDoc(doc(firestore, "users", uid), {
         displayName,
+        uploadKey: generateUploadKey(),
         createdAt: new Date().getTime(),
     });
 };
