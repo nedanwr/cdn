7aab385de6945aa86d9866cbf11a40aa836ffbce
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-10 12:17:47 +0200

    Feat: add storage bucket url

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -3,7 +3,7 @@ import { FirebaseStorage, StorageReference } from "firebase/storage";
 import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 export const uploadImage = async (fileName: string, file: Express.Multer.File) => {
-    const storage: FirebaseStorage = getStorage(app, "");
+    const storage: FirebaseStorage = getStorage(app, process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL!);
     const storageRef: StorageReference = ref(storage, `${fileName}.${file.mimetype.split("/")[1]}`);
 
     return uploadBytesResumable(storageRef, file.buffer, {
